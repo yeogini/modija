e0ec0862870425604153a7d59e0a6adf9624c8ee
yeogini <ywkim@wkit.co.kr>
2022-04-20 16:02:50 +0900

    최초 파일 생성

---
+++ backend/src/main/java/modija/modija/ModijaApplication.java
@@ -0,0 +1,13 @@
+package modija.modija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModijaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModijaApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/modija/modija/ModijaApplicationTests.java
@@ -0,0 +1,13 @@
+package modija.modija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModijaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
