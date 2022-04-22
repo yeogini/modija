a9d1a8e178aaaa7ab9e76d7318d19e3a8ceb7202
yeogini <ywkim@wkit.co.kr>
2022-04-22 14:12:58 +0900

    h2 데이터베이스

--- backend/src/main/java/modija/modija/ModijaApplication.java
+++ backend/src/main/java/modija/modija/ModijaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ModijaApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.profiles.default", "h2");
         SpringApplication.run(ModijaApplication.class, args);
     }
 
---
+++ backend/src/main/resources/sample/data.sql
@@ -0,0 +1,9 @@
+insert into "MEMBER"
+(id, account, contact, name, password, reg_dt)
+values
+(1, 'test1', '01012345678', '테스트1', 'password', now());
+
+insert into "MEMBER"
+(id, account, contact, name, password, reg_dt)
+values
+(2, 'test2', '01067891234', '테스트2', 'password', now());
