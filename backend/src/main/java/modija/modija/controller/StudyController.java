2926fcb5ea5102ba9f36bc627ba399cf873ca169
yeogini <oogochi2@gmail.com>
2022-06-02 17:54:02 +0900

    ResponseEntity 사용

--- backend/src/main/java/modija/modija/controller/StudyController.java
+++ backend/src/main/java/modija/modija/controller/StudyController.java
@@ -1,6 +1,13 @@
 package modija.modija.controller;
 
 import lombok.RequiredArgsConstructor;
+import modija.modija.domain.study.Study;
+import modija.modija.service.StudyService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/study")
 public class StudyController {
+
+    private final StudyService studyService;
+
+
+    @GetMapping("/")
+    public ResponseEntity list() {
+
+        int page = 0;
+        int size = 5;
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Study> studys = studyService.findAll(pageable);
+
+        return ResponseEntity.ok(studys);
+    }
+
+
 }
